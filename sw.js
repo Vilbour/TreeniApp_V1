9278ea48373c0101225fc8ab5d5c674332a5bcda
Vilbour <61020110+Vilbour@users.noreply.github.com>
2025-08-26 07:00:53 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,11 @@
-const CACHE = 'treeni-v4';
+const CACHE = 'treeniapp-cache-v1';
 const ASSETS = ['index.html','style.css','app.js','manifest.json'];
-
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
-self.addEventListener('activate', e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
 });
-
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
